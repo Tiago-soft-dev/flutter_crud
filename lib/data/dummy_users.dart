ad1be523d640a82f2a4900312c9dd44ab42f80a2
tiagoneppi129 <tiagoneppi129@gmail.com>
2025-10-21 16:00:47 +0000

    correção nome

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,21 +1,21 @@
 import 'package:myapp/models/user.dart';
 
-const DUMMY_USERS = {
-  '1': const User(
+const dummyUsers = {
+  '1':  User(
     id: '1',
     name: 'Maria',
     email: 'maria@maria.com',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2023/03/31/05/52/avatar-7889246_640.jpg',
   ),
-  '2': const User(
+  '2':  User(
     id: '2',
     name: 'João',
     email: 'joao@joao.com',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2016/04/01/10/04/amusing-1299754_640.png',
   ),
-  '3': const User(
+  '3':  User(
     id: '3',
     name: 'Pedro',
     email: 'pedro@pedro.com',
